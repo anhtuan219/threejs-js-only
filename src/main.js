c22deff43e7b9904833b375fad70af4c48594160
anhtuan219 <tuanbulba@gmail.com>
2026-08-09 14:50:00 +0700

    create objects

--- src/main.js
+++ src/main.js
@@ -15,11 +15,29 @@ const camera = new THREE.PerspectiveCamera(
   1000, // far clipping plane: objects further away from the camera than the value of `far` won't be rendered
 );
 
+// Create cube object
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+});
+// Objects created from geometry and material.
+const cube = new THREE.Mesh(geometry, material);
+// by default, objects will be added to the coordinates (0,0,0)
+// cause both the camera and the objects to be inside each other
+scene.add(cube);
+// to avoid this, move the camera out a bit
+camera.position.z = 5;
+
 // RENDERER
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(windowWidth, windowHeight);
-// Rendering the scene
-renderer.render(scene, camera);
+function animate(time) {
+  cube.rotation.x = time / 2000;
+  cube.rotation.y = time / 1000;
+  // Rendering the scene
+  renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
 
 // Add the renderer's DOM element - a canvas to the web page
 document.body.appendChild(renderer.domElement);
